1085f168a3a39b912d8dc8632f101afea0f7b67d
plonetti <lonetti@paciunzio.istruzioneer.it>
2024-02-17 10:31:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import gpxpy
-#import gpxpy.gpx
 import os 
 import pandas as pd
 dir='DATI'
 lista=[['Name','Total distance','Total time','Max speed','Data']]
-#dir_list = os.listdir(dir)
-#print(dir_list)
 for filename in os.listdir(dir): 
     if filename.endswith(".gpx"):
         file_path = os.path.join(dir, filename)
-        #os.chdir(r'./DATI')
         print (filename)
         with open (file_path,'r') as gpx_file:
             gpx = gpxpy.parse(gpx_file)
